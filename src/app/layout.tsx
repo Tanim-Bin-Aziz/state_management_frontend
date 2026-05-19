d0d933b8be04233bbb2bf1a0ce5c0e7119f241e2
Tanim Bin Aziz <tanimbinaziz145@gmail.com>
2026-05-19 13:46:38 +0600

    ui uodate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className="min-h-screen bg-linear-to-b from-[#ffe5c2] "
-        suppressHydrationWarning={true}
-      >
+      <body className="min-h-screen bg-white " suppressHydrationWarning={true}>
         <main>{children}</main>
       </body>
     </html>
